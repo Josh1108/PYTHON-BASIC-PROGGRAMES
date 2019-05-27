f8fd8a70cc454150e755def81a7e249172166a86
Josh1108 <49173968+Josh1108@users.noreply.github.com>
2019-05-27 10:06:13 +0530

    Update BlackJack.py

--- BlackJack.py
+++ BlackJack.py
@@ -223,3 +223,102 @@ def hit():
         result = "Sorry, can't hit" + restart_phrase
     
     game_step()
+def stand():
+    global playing,chip_pool,deck,player_hand,dealer_hand,result,bet
+    ''' This function will now play the dealers hand, since stand was chosen '''
+    
+    if playing == False:
+        if player_hand.calc_val() > 0:
+            result = "Sorry, you can't stand!"
+            
+    # Now go through all the other possible options
+    else:
+        
+        # Soft 17 rule
+        while dealer_hand.calc_val() < 17:
+            dealer_hand.card_add(deck.deal())
+            
+        # Dealer Busts    
+        if dealer_hand.calc_val() > 21:
+            result = 'Dealer busts! You win!' + restart_phrase
+            chip_pool += bet
+            playing = False
+            
+        #Player has better hand than dealer
+        elif dealer_hand.calc_val() < player_hand.calc_val():
+            result = 'You beat the dealer, you win!' + restart_phrase
+            chip_pool += bet
+            playing = False
+        
+        # Push
+        elif dealer_hand.calc_val() == player_hand.calc_val():
+            result = 'Tied up, push!' + restart_phrase
+            playing = False
+        
+        # Dealer beats player
+        else:
+            result = 'Dealer Wins!' + restart_phrase
+            chip_pool -= bet
+            playing = False
+    game_step()
+def game_step():
+    'Function to print game step/status on output'
+    
+    #Display Player Hand
+    print ""
+    print('Player Hand is: '),
+    player_hand.draw(hidden =False)
+    
+    print 'Player hand total is: '+str(player_hand.calc_val())
+    
+    #Display Dealer Hand
+    print('Dealer Hand is: '),
+    dealer_hand.draw(hidden=True)
+    
+    # If game round is over
+    if playing == False:
+        print  " --- for a total of " + str(dealer_hand.calc_val() )
+        print "Chip Total: " + str(chip_pool)
+    # Otherwise, don't know the second card yet
+    else: 
+        print " with another card hidden upside down"
+    
+    # Print result of hit or stand.
+    print result
+    
+    player_input()
+def game_exit():
+    print 'Thanks for playing!'
+    exit()
+def player_input():
+    ''' Read user input, lower case it just to be safe'''
+    plin = raw_input().lower()
+    
+    
+    if plin == 'h':
+        hit()
+    elif plin == 's':
+        stand()
+    elif plin == 'd':
+        deal_cards()
+    elif plin == 'q':
+        game_exit()
+    else:
+        print "Invalid Input...Enter h, s, d, or q: "
+        player_input()
+def intro():
+    statement = '''Welcome to BlackJack! Get as close to 21 as you can without going over!
+    Dealer hits until she reaches 17. Aces count as 1 or 11.
+    Card output goes a letter followed by a number of face notation'''
+    print statement
+# Create a Deck
+deck = Deck()
+#Shuffle it
+deck.shuffle()
+# Create player and dealer hands
+player_hand = Hand()
+dealer_hand = Hand()
+#Print the intro
+intro()
+# Deal out the cards and start the game!
+deal_cards()
